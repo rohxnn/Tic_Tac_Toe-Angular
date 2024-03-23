dc5a9fa7af58fda7c86753af4e485fe09dbbc0ab
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-23 21:24:45 +0530

    feat(game): add function new game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  squares: any[];
+  xIsNext: boolean;
+  winner: string;
 
+  ngOnInit(){
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+  
 }
