957dd9802428c73512c486a34fc2190e1471ae0c
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-23 17:48:10 +0530

    feat(game): add new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//component
+import { GameComponent } from './game/game.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
